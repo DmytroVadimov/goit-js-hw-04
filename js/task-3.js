2932e0ef78061439a693a85e464c136078fbd1a4
Dmytro Vadimov <104729263+DmytroVadimov@users.noreply.github.com>
2024-03-14 21:37:18 +0200

    task_3_start

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,17 @@
 'use strict';
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  //changeUsername(newName)
+  //updatePlayTime(hours)
+  //getInfo()
+  //<Username> has <amount> active hours!
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
