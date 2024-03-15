648f0c9ee0687ff32e5362cdebcd52ed5b415d77
Dmytro Vadimov <104729263+DmytroVadimov@users.noreply.github.com>
2024-03-15 13:58:06 +0200

    task_3

--- js/task-3.js
+++ js/task-3.js
@@ -2,16 +2,22 @@
 const profile = {
   username: 'Jacob',
   playTime: 300,
-  //changeUsername(newName)
-  //updatePlayTime(hours)
-  //getInfo()
-  //<Username> has <amount> active hours!
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    console.log(`${this.username} has ${this.playTime} active hours!`);
+  },
 };
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.getInfo(); // "Jacob has 300 active hours!"
 
 profile.changeUsername('Marco');
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.getInfo(); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+profile.getInfo(); // "Marco has 320 active hours!"
